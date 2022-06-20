614862eb5570e355ca6797a1e8f8c4a16da4e5a4
Adham Atef <adham.atef45@gmail.com>
2022-06-21 01:46:26 +0200

    Sarting database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_to_do/services/theme_services.dart';
 import 'package:flutter_to_do/ui/home_page.dart';
 import 'package:flutter_to_do/ui/themes.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
@@ -12,12 +17,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-theme: Themes.light,
-      themeMode: ThemeMode.dark,
-darkTheme: Themes.dark,
+      theme: Themes.light,
+      themeMode: ThemeService().theme,
+      darkTheme: Themes.dark,
       home: HomePage(),
     );
   }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,5 +1,14 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_to_do/services/notification_services.dart';
+import 'package:flutter_to_do/services/theme_services.dart';
+import 'package:flutter_to_do/ui/add_task_page.dart';
+import 'package:flutter_to_do/ui/themes.dart';
+import 'package:flutter_to_do/ui/widgets/button.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,40 +18,122 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime _selectedDate = DateTime.now();
+  var notifyHelper;
+
+  @override
+  void initState() {
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.initializeNotification();
+    notifyHelper.requestIOSPermissions();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: context.theme.backgroundColor,
       appBar: _appBar(),
       body: Column(
         children: [
-          Text(
-            "Theme Data",
-            style: TextStyle(
-              fontSize: 30,
-            ),
-          ),
+          _addTaskBar(),
+          _addDateBar(),
         ],
       ),
     );
   }
-
-  _appBar() {
+_addTaskBar() {
+    return Container(
+      margin: EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat.yMMMd().format(
+                  DateTime.now(),
+                ),
+                style: subHeadingStyle,
+              ),
+              Text(
+                "Today",
+                style: headingStyle,
+              ),
+            ],
+          ),
+          MyButton(label: "+ Add Task", onTap: () => Get.to(AddTaskPage()))
+        ],
+      ),
+    );
+  }
+_addDateBar(){
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 20,
+        left: 20,
+      ),
+      child: DatePicker(
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor: primaryClr,
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+            fontSize: 20,
+          ),
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+            fontSize: 16,
+          ),
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: TextStyle(
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+            fontSize: 14,
+          ),
+        ),
+        onDateChange: (date) {
+          _selectedDate = date;
+        },
+      ),
+    );
+}
+_appBar() {
     return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.backgroundColor,
       leading: GestureDetector(
         onTap: () {
           if (kDebugMode) {
-            print("tapped");
+            ThemeService().switchTheme();
+            notifyHelper.displayNotification(
+              title: "Theme Changed",
+              body: Get.isDarkMode
+                  ? "Activated Light Theme"
+                  : "Activated Dark Theme",
+            );
+            notifyHelper.scheduledNotification();
           }
         },
         child: Icon(
-          Icons.nightlight_round,
+          Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round,
           size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
         ),
       ),
       actions: [
-        Icon(
-          Icons.person,
-          size: 20,
+        CircleAvatar(
+          backgroundImage: AssetImage("assets/images/profile.png"),
         ),
         SizedBox(
           width: 20,
